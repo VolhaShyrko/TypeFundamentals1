a6b44d513a54d6567282d9992643e1e32b95849a
VolhaShyrko <olgashyrko@gmail.com>
2016-08-11 11:26:51 +0300

    first commit

---
+++ TypeFundamentals1/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace TypeFundamentals1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public bool Equals(Person obj)
+        {
+            if (obj == null) return false;
+
+            return obj.FirstName == this.FirstName && obj.LastName == this.LastName;
+        }
+
+        public override int GetHashCode()
+        {
+            var a = this.FirstName.GetHashCode();
+            var b = this.LastName.GetHashCode();
+            return a ^ b;
+        }
+    }
+}
---
+++ TypeFundamentals1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TypeFundamentals1
+{
+    using System;
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var p1 = new Person("Alex", "Ivanov");
+            var p2 = new Person("Alex", "Ivanov");
+
+            Console.WriteLine(p1.Equals(p2));
+            Console.ReadKey();
+        }
+    }
+}
+
+
